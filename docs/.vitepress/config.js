37bac85273b0f647ebde375af7e26ff161664604
Adam Jedlička <mail@adamjedlicka.cz>
2022-05-15 16:34:26 +0200

    Add docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+const config = {
+  title: 'Fir.js',
+  description: 'Framework agnostic meta-framework for server-side rendered applications',
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]],
+}
+
+export default config
